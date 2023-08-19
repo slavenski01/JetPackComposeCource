b9dde40f541ea63f28ba4aedccee0ac33daf1ba1
vyacheslav kuznetsov <vyachekuznetsov@x5.ru>
2023-08-19 17:35:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposecource/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.jetpackcomposecource
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimesTable()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) {
+                        Color.Yellow
+                    } else {
+                        Color.White
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .background(color)
+                            .border(width = 1.dp, color = Color.DarkGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = (i * j).toString())
+                    }
+                }
+            }
+        }
+    }
+}
