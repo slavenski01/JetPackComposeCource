052f7004456937485a7e27d115c61b4781a07f18
vyacheslav kuznetsov <vyachekuznetsov@x5.ru>
2023-08-21 23:51:36 +0300

    DarkTheme

--- app/src/main/java/com/example/jetpackcomposecource/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecource/MainActivity.kt
@@ -3,36 +3,27 @@ package com.example.jetpackcomposecource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import com.example.jetpackcomposecource.ui.theme.InstagramCard
+import com.example.jetpackcomposecource.ui.theme.JetPackComposeCourceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardTest()
+            JetPackComposeCourceTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    InstagramCard()
+                }
+            }
         }
-    }
-}
-
-@Preview
-@Composable
-fun CardTest() {
-    Card(
-        shape = RoundedCornerShape(
-            topStart = 4.dp,
-            topEnd = 4.dp
-        ),
-        colors = CardDefaults.cardColors(Color.White),
-        border = BorderStroke(1.dp, color = Color.Black)
-    ) {
-        InstagramCard()
     }
 }
--- app/src/main/java/com/example/jetpackcomposecource/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/jetpackcomposecource/ui/theme/InstagramProfileCard.kt
@@ -1,5 +1,7 @@
 package com.example.jetpackcomposecource.ui.theme
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,18 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview
 @Composable
 fun InstagramCard() {
     HeaderCard(
@@ -34,11 +34,13 @@ fun InstagramCard() {
 @Composable
 fun HeaderCard(subInfoList: List<Pair<String, String>>) {
     Card(
+        modifier = Modifier.padding(all = 8.dp),
         shape = RoundedCornerShape(
             topStart = 8.dp,
             topEnd = 8.dp
         ),
-        colors = CardDefaults.cardColors(Color.White)
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
     ) {
         Row(
             modifier = Modifier
@@ -80,5 +82,25 @@ private fun SubsInfo(countString: String, name: String) {
                 fontSize = 8.sp
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun InstagramDarkPreview() {
+    JetPackComposeCourceTheme(
+        darkTheme = true
+    ) {
+        InstagramCard()
+    }
+}
+
+@Preview
+@Composable
+fun InstagramLightPreview() {
+    JetPackComposeCourceTheme(
+        darkTheme = false
+    ) {
+        InstagramCard()
     }
 }
